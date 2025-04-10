d4d5384d11fbd2bd2b54f027f5f7062dfdc1ad56
paprikadreamdetective <t4v0sg@gmail.com>
2025-04-09 23:55:40 -0600

    fix: panels dissapear

--- src/forms/Dashboard.java
+++ src/forms/Dashboard.java
@@ -28,6 +28,9 @@ public class Dashboard extends javax.swing.JFrame {
     // yellow border for the menu items
     Border yellow_border = BorderFactory.createMatteBorder(1, 0, 1, 0, Color.YELLOW);
     
+    javax.swing.JLabel[] menuLabels;
+    javax.swing.JPanel[] panels;
+    
     public Dashboard() {
         initComponents();
         // center this form
@@ -50,6 +53,12 @@ public class Dashboard extends javax.swing.JFrame {
         menuLabels[3] = Label_menuItem4;
         menuLabels[4] = Label_menuItem5;
         menuLabels[5] = Label_menuItem6;
+        
+        /*for (JLabel label : menuLabels) {
+            if (label != null) {
+                label.setOpaque(true);
+            }
+        }*/
         
         panels = new javax.swing.JPanel[2];
         
@@ -89,7 +98,9 @@ public class Dashboard extends javax.swing.JFrame {
         Label_menuItem5 = new javax.swing.JLabel();
         Label_menuItem6 = new javax.swing.JLabel();
         jPanel1_dashboard = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
         jPanel2_users = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -129,80 +140,99 @@ public class Dashboard extends javax.swing.JFrame {
         jPanel_menu.setBackground(new java.awt.Color(51, 51, 51));
 
         Label_menuItem1.setForeground(new java.awt.Color(255, 255, 255));
+        Label_menuItem1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Label_menuItem1.setText("Dashboard");
+        Label_menuItem1.setHorizontalTextPosition(javax.swing.SwingConstants.LEADING);
 
         Label_menuItem2.setForeground(new java.awt.Color(255, 255, 255));
+        Label_menuItem2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Label_menuItem2.setText("Usuarios");
 
         Label_menuItem3.setForeground(new java.awt.Color(255, 255, 255));
+        Label_menuItem3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Label_menuItem3.setText("Registo de asistentes");
 
         Label_menuItem4.setForeground(new java.awt.Color(255, 255, 255));
+        Label_menuItem4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Label_menuItem4.setText("Inscripcion de asistentes");
 
         Label_menuItem5.setForeground(new java.awt.Color(255, 255, 255));
+        Label_menuItem5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Label_menuItem5.setText("Información de los asistentes");
 
         Label_menuItem6.setForeground(new java.awt.Color(255, 255, 255));
+        Label_menuItem6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Label_menuItem6.setText("Exportar");
 
         javax.swing.GroupLayout jPanel_menuLayout = new javax.swing.GroupLayout(jPanel_menu);
         jPanel_menu.setLayout(jPanel_menuLayout);
         jPanel_menuLayout.setHorizontalGroup(
             jPanel_menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel_menuLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel_menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Label_menuItem3)
-                    .addComponent(Label_menuItem2)
-                    .addComponent(Label_menuItem1)
-                    .addComponent(Label_menuItem4)
-                    .addComponent(Label_menuItem5)
-                    .addComponent(Label_menuItem6))
-                .addContainerGap(14, Short.MAX_VALUE))
+            .addComponent(Label_menuItem1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(Label_menuItem2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(Label_menuItem3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(Label_menuItem4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(Label_menuItem5, javax.swing.GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE)
+            .addComponent(Label_menuItem6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel_menuLayout.setVerticalGroup(
             jPanel_menuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel_menuLayout.createSequentialGroup()
                 .addContainerGap(230, Short.MAX_VALUE)
-                .addComponent(Label_menuItem1)
-                .addGap(49, 49, 49)
-                .addComponent(Label_menuItem2)
-                .addGap(54, 54, 54)
-                .addComponent(Label_menuItem3)
-                .addGap(51, 51, 51)
-                .addComponent(Label_menuItem4)
-                .addGap(41, 41, 41)
-                .addComponent(Label_menuItem5)
-                .addGap(44, 44, 44)
-                .addComponent(Label_menuItem6)
-                .addGap(107, 107, 107))
+                .addComponent(Label_menuItem1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Label_menuItem2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Label_menuItem3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(Label_menuItem4, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(Label_menuItem5, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(Label_menuItem6, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(94, 94, 94))
         );
 
         jPanel_container.add(jPanel_menu, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 210, 690));
+
+        jLabel2.setText("DASHBOARD");
 
         javax.swing.GroupLayout jPanel1_dashboardLayout = new javax.swing.GroupLayout(jPanel1_dashboard);
         jPanel1_dashboard.setLayout(jPanel1_dashboardLayout);
         jPanel1_dashboardLayout.setHorizontalGroup(
             jPanel1_dashboardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1020, Short.MAX_VALUE)
+            .addGroup(jPanel1_dashboardLayout.createSequentialGroup()
+                .addGap(461, 461, 461)
+                .addComponent(jLabel2)
+                .addContainerGap(473, Short.MAX_VALUE))
         );
         jPanel1_dashboardLayout.setVerticalGroup(
             jPanel1_dashboardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 630, Short.MAX_VALUE)
+            .addGroup(jPanel1_dashboardLayout.createSequentialGroup()
+                .addGap(273, 273, 273)
+                .addComponent(jLabel2)
+                .addContainerGap(338, Short.MAX_VALUE))
         );
 
         jPanel_container.add(jPanel1_dashboard, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 60, 1020, 630));
+
+        jLabel3.setText("USUARIOS");
 
         javax.swing.GroupLayout jPanel2_usersLayout = new javax.swing.GroupLayout(jPanel2_users);
         jPanel2_users.setLayout(jPanel2_usersLayout);
         jPanel2_usersLayout.setHorizontalGroup(
             jPanel2_usersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1020, Short.MAX_VALUE)
+            .addGroup(jPanel2_usersLayout.createSequentialGroup()
+                .addGap(473, 473, 473)
+                .addComponent(jLabel3)
+                .addContainerGap(478, Short.MAX_VALUE))
         );
         jPanel2_usersLayout.setVerticalGroup(
             jPanel2_usersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 630, Short.MAX_VALUE)
+            .addGroup(jPanel2_usersLayout.createSequentialGroup()
+                .addGap(286, 286, 286)
+                .addComponent(jLabel3)
+                .addContainerGap(325, Short.MAX_VALUE))
         );
 
         jPanel_container.add(jPanel2_users, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -228,14 +258,25 @@ public class Dashboard extends javax.swing.JFrame {
         this.dispose();
     }                                         
 
-    public void setLabelBackround(javax.swing.JLabel label) {
+    /*public void setLabelBackround(javax.swing.JLabel label) {
         for (javax.swing.JLabel menuItem : menuLabels) {
             menuItem.setBackground(new Color(46,49,49));
             menuItem.setForeground(Color.white); 
         }
         label.setBackground(Color.white);
         label.setForeground(Color.blue);
+    }*/
+    
+    public void setLabelBackround(javax.swing.JLabel label) {
+    for (javax.swing.JLabel menuItem : menuLabels) {
+        menuItem.setOpaque(true); // Asegurarse de que se puede pintar el fondo
+        menuItem.setBackground(new Color(51, 51, 51)); // Fondo por defecto
+        menuItem.setForeground(Color.WHITE); 
     }
+    label.setOpaque(true);
+    label.setBackground(Color.WHITE); // Fondo cuando está seleccionado
+    label.setForeground(Color.BLUE);  // Texto azul
+}
     
     public void showPanel(javax.swing.JPanel panel) {
         for (javax.swing.JPanel pnl : panels) {
@@ -248,18 +289,24 @@ public class Dashboard extends javax.swing.JFrame {
         Component[] components = jPanel_menu.getComponents();
 
         for (Component component : components) {
-            if (component instanceof javax.swing.JLabel) {
-                javax.swing.JLabel label = (javax.swing.JLabel) component;
+            if (component instanceof javax.swing.JLabel label) {
 
                 label.addMouseListener(new MouseListener() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
                         setLabelBackround(label);
                         switch (label.getText().trim()) {
-                            case "Dashboard":
-                                showPanel(jPanel1_dashboard); break;
-                            case "Users":
-                                showPanel(jPanel2_users); break;
+                            case "Dashboard" -> showPanel(jPanel1_dashboard);
+                            case "Users" -> showPanel(jPanel2_users);
+                            /*case "Products":
+                            showPanel(jPanel_products); break;
+                            case "Settings":
+                            showPanel(jPanel_settings); break;
+                            case "Contact":
+                            showPanel(jPanel_contact); break;
+                            case "Calendar":
+                            showPanel(jPanel_calendar); break;
+                            case "Test":
                             /*case "Products":
                                 showPanel(jPanel_products); break;
                             case "Settings":
@@ -328,6 +375,8 @@ public class Dashboard extends javax.swing.JFrame {
     private javax.swing.JLabel Label_menuItem5;
     private javax.swing.JLabel Label_menuItem6;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel_appLogo;
     private javax.swing.JPanel jPanel1_dashboard;
     private javax.swing.JPanel jPanel2_users;
@@ -335,6 +384,5 @@ public class Dashboard extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel_logoANDname;
     private javax.swing.JPanel jPanel_menu;
     // End of variables declaration//GEN-END:variables
-    private javax.swing.JLabel[] menuLabels;
-    private javax.swing.JPanel[] panels;
+    
 }
